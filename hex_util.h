d31bf5cff7f07fdb5082dab9730f3fd06e9224c0
logcho <lsjcwma@gmail.com>
2025-01-16 13:16:46 -0600

    feat: implementation

--- hex_util.h
+++ hex_util.h
@@ -7,23 +7,23 @@
 
 std::string stringToHex(const std::string& input);
 
-std::string hexToString(const std::string& input);
+std::string hexToString(const std::string& hexInput);
 
 std::string intToHex(int number);
 
-int hexToInt(const std::string& input);
+int hexToInt(const std::string& hexInput);
 
 std::string vectorToHexUint8(const std::vector<uint8_t>& bytes);
 
-std::vector<uint8_t> hexToVectorUint8(const std::string& input);
+std::vector<uint8_t> hexToVectorUint8(const std::string& hexInput);
 
-std::string vectorToHexUint16(const std::vector<uint16_t>& bytes);
+std::string vectorToHexUint16(const std::vector<uint16_t>& values);
 
-std::vector<uint16_t> hexToVectorUint16(const std::string& input);
+std::vector<uint16_t> hexToVectorUint16(const std::string& hexInput);
 
-std::string vectorToHexUint32(const std::vector<uint32_t>& bytes);
+std::string vectorToHexUint32(const std::vector<uint32_t>& values);
 
-std::vector<uint32_t> hexToVectorUint32(const std::vector<uint32_t>& bytes);
+std::vector<uint32_t> hexToVectorUint32(const std::string& hexToInt);
 
 
 #endif // HEX_UTIL_H
